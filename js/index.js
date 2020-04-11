dc52fe1a1253051d05a2c299e3eb9258809ec8f2
Ahmedmubarakweb <58855051+Ahmedmubarakweb@users.noreply.github.com>
2020-04-11 04:40:23 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,69 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    },
+    960: {
+        items: 2
+    }
+  }
+  $('.cards').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow:4 ,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+     //show scroll button 
+     var btnScroll= $(".move-up");
+     btnScroll.hide();
+     $(window).scroll(function(){
+       if($(this).scrollTop()>1000){
+        btnScroll.show();
+       }
+       else
+       btnScroll.hide();
+     })
