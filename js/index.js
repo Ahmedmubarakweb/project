96310e9ad227e7248ee638d9b8d9f1685f30c7d0
Ahmedmubarakweb <58855051+Ahmedmubarakweb@users.noreply.github.com>
2020-04-12 06:15:09 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,8 +17,8 @@ const responsive = {
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow:4 ,
-    slidesToScroll: 2,
+    slidesToShow:3 ,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 992,
